3db73b7a9f82dd531e904be8a836faee14d52a42
CraftYouMe <651446044@qq.com>
2023-04-05 00:07:38 +0800

    默认关闭联系上下文

--- src/store/modules/chat/helper.ts
+++ src/store/modules/chat/helper.ts
@@ -6,7 +6,7 @@ export function defaultState(): Chat.ChatState {
   const uuid = 1002
   return {
     active: uuid,
-    usingContext: true,
+    usingContext: false,
     history: [{ uuid, title: 'New Chat', isEdit: false }],
     chat: [{ uuid, data: [] }],
   }
